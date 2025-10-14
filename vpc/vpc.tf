2fdaf4308ea85c21570c5bb72619507cbd793707
addis <addis.h.gebru@gmail.com>
2025-10-14 15:53:20 -0400

    deleted vpc code lines

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,13 +1,5 @@
 #this is where you come to reference declared variables
 #create vpc
-resource "aws_vpc" "main_vpc" {
-  cidr_block       = var.vpc_cidr_block
-  instance_tenancy = "default"
-
-  tags = merge(var.tags, {
-    Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}main-vpc"
-})
-}
 
 #create 2 database subnets 
 resource "aws_subnet" "apci_jupiter_db_subnet_az_1c" {
